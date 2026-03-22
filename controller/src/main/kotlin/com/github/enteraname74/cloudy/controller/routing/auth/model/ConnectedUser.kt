2073206eb52f8f120f19b6ed34c62f7d088d8dbd
enteraname74 <noahpenin@gmail.com>
2026-03-22 01:13:45 +0100

    Adjustments

--- controller/src/main/kotlin/com/github/enteraname74/cloudy/controller/routing/auth/model/ConnectedUser.kt
+++ controller/src/main/kotlin/com/github/enteraname74/cloudy/controller/routing/auth/model/ConnectedUser.kt
@@ -2,15 +2,18 @@ package com.github.enteraname74.cloudy.controller.routing.auth.model
 
 import com.github.enteraname74.cloudy.domain.model.User
 import kotlinx.serialization.Serializable
+import kotlin.uuid.Uuid
 
 @Serializable
 data class ConnectedUser(
+    val id: Uuid,
     val username: String,
     val isAdmin: Boolean,
 )
 
 fun User.toConnectedUser(): ConnectedUser =
     ConnectedUser(
+        id = id,
         username = username,
         isAdmin = isAdmin,
     )
