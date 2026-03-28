f661ee7009dbee17d601ab8aa060da3e985c63a3
enteraname74 <noahpenin@gmail.com>
2026-03-28 16:01:32 +0100

    Add support for partial content

--- controller/build.gradle.kts
+++ controller/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation(libs.ktor.server.resources)
     implementation(libs.ktor.server.request.validation)
     implementation(libs.ktor.server.status.page)
+    implementation(libs.ktor.server.partial.content)
 
     implementation(libs.ktor.simple.cache)
     implementation(libs.ktor.server.netty)
--- controller/src/main/kotlin/com/github/enteraname74/cloudy/controller/routing/music/routes/GetMusicFile.kt
+++ controller/src/main/kotlin/com/github/enteraname74/cloudy/controller/routing/music/routes/GetMusicFile.kt
@@ -6,22 +6,35 @@ import com.github.enteraname74.cloudy.controller.ext.missingTokenInformation
 import com.github.enteraname74.cloudy.controller.ext.response
 import com.github.enteraname74.cloudy.controller.routing.music.resource.MusicResource
 import com.github.enteraname74.cloudy.domain.service.MusicService
+import com.github.enteraname74.cloudy.logging.cloudyLogger
 import io.ktor.http.ContentDisposition
 import io.ktor.http.ContentType
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpStatusCode
 import io.ktor.http.defaultForFile
+import io.ktor.server.plugins.partialcontent.PartialContent
 import io.ktor.server.resources.get
 import io.ktor.server.response.header
 import io.ktor.server.response.respondFile
 import io.ktor.server.routing.Route
+import io.ktor.server.routing.contentType
 import org.koin.ktor.ext.inject
 import java.io.File
 
 fun Route.getMusicFile() {
     val musicService by inject<MusicService>()
 
+    install(PartialContent)
+
     get<MusicResource.File> { musicResource ->
+        cloudyLogger.debug("Get file requested")
+        call.request.headers.forEach { key, values ->
+            cloudyLogger.debug("request header: $key -- $values")
+        }
+
+        val range = call.request.headers[HttpHeaders.Range]
+        cloudyLogger.debug("range header? $range")
+
         val username: String = getUsernameFromToken() ?: return@get missingTokenInformation()
 
         val musicFile: File = musicService.getMusicFile(
